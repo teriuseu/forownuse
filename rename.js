3472e4ab0eb050f6e94ecb86ad26d2fab695b1f7
teriuseu <115878300+teriuseu@users.noreply.github.com>
2023-02-26 10:12:53 +0800

    Update rename.js

--- rename.js
+++ rename.js
@@ -2,7 +2,7 @@
 // 感谢futurkk (@futurkk)
 
 //增加前缀
-//复制这条：https://raw.githubusercontent.com/Twoandz9/Tool-Surge/main/module/rename.js#input=zh&output=zh&airport=前缀
+//复制这条：https://raw.githubusercontent.com/teriuseu/forownuse/main/rename.js#input=zh&output=zh&airport=机场名
 
 // Rename v 0.5 的使用方法
 // 在以下十二个连接中选择你原节点的格式及你需要的格式复制并修改[&airport=]部分即可。
